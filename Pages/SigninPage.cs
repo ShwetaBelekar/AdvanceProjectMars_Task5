e363349ed73ae161388ca007251676f7dfee90b2
Shweta Belekar <belekarshwetagmail.com>
2025-08-29 11:29:24 +1200

    Automated tests for signin page

--- Pages/SigninPage.cs
+++ Pages/SigninPage.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AdvanceProjectMars_Task5.Utilities;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,26 @@ using System.Threading.Tasks;
 
 namespace AdvanceProjectMars_Task5.Pages
 {
-    internal class SigninPage
+    public class SigninPage : CommonDriver
     {
+        public void ValidSigninActions()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:5003/Home");
+            driver.Manage().Window.Maximize();
+
+            IWebElement signInButton = driver.FindElement(By.XPath("//*[@id=\"home\"]/div/div/div[1]/div/a"));
+            signInButton.Click();
+
+            IWebElement emailAddressTextbox = driver.FindElement(By.XPath("//input[@placeholder='Email address']"));
+            emailAddressTextbox.SendKeys("moneytony@ymail.com");
+
+            IWebElement passwordTextbox = driver.FindElement(By.XPath("//input[@placeholder='Password']"));
+            passwordTextbox.SendKeys("Tonymoney@2025");
+
+            IWebElement
+
+
+        }
     }
 }
