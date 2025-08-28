72d37b2f11619fa34ac98f8df90b89379f25bc3f
Shweta Belekar <belekarshwetagmail.com>
2025-08-28 12:04:26 +1200

    Automate tests for Project Mars features

--- Program.cs
+++ Program.cs
@@ -1,2 +1,95 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Drawing;
+using System.Xml.Linq;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("http://localhost:5003/Home");
+        driver.Manage().Window.Maximize();
+
+        IWebElement signInButton = driver.FindElement(By.XPath("//*[@id=\"home\"]/div/div/div[1]/div/a"));
+        signInButton.Click();
+
+        IWebElement emailAddressTextbox = driver.FindElement(By.XPath("//input[@placeholder='Email address']"));
+        emailAddressTextbox.Click();
+        emailAddressTextbox.SendKeys("moneytony@ymail.com");
+
+        IWebElement passwordTextbox = driver.FindElement(By.XPath("//input[@placeholder='Password']"));
+        passwordTextbox.Click();
+        passwordTextbox.SendKeys("Tonymoney@2025");
+
+        IWebElement loginButton = driver.FindElement(By.XPath("//button[@class='fluid ui teal button']"));
+        loginButton.Click();
+        Thread.Sleep(5000);
+
+        IWebElement hiTony = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/div[1]/div[2]/div/span"));
+
+        if (hiTony.Text == "Hi Tony")
+        {
+            Console.WriteLine("User has logged in Successfully. Test Passed!");
+        }
+        else
+        {
+            Console.WriteLine("User has not logged in. Test Failed!");
+        }
+
+        IWebElement profileTab = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[1]/div/a[2]"));
+        profileTab.Click();
+        Thread.Sleep(3000);
+        IWebElement locationButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[2]/div/div/div/div/div/div[3]/div/div[1]/div"));
+        if (locationButton.Enabled)
+        {
+            Console.WriteLine("Location button is enabled, but it should not be.");
+        }
+        else
+        {
+            Console.WriteLine("Location button is not enabled, as expected.");
+        }
+
+        IWebElement availabilityEditButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[2]/div/div/div/div/div/div[3]/div/div[2]/div/span/i"));
+        availabilityEditButton.Click(); 
+
+        IWebElement availabilityDropdownButton = driver.FindElement(By.XPath("//select[@name='availabiltyType']"));
+        availabilityDropdownButton.Click();
+        //Thread.Sleep(3000);
+
+        IWebElement availabilityTypeOption = driver.FindElement(By.XPath("//option[contains(text(), 'Part Time') and @value='0']"));
+        availabilityTypeOption.Click();
+
+        //IWebElement availabilityTypeOption = driver.FindElement(By.XPath("//option[contains(text(), 'Full Time') and @value='1']"));
+        //availabilityTypeOption.Click();
+
+        IWebElement popupAlert = driver.FindElement(By.XPath("//div[@class='ns-box ns-growl ns-effect-jelly ns-type-success ns-show']"));
+        if(popupAlert.Text == "Availability updated")
+        {
+            Console.WriteLine("Availability updated successfully");
+        }
+        else
+        {
+            Console.WriteLine("Availability updated unsuccessfull");
+        }
+
+        IWebElement hoursEditButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[2]/div/div/div/div/div/div[3]/div/div[3]/div/span/i"));
+        hoursEditButton.Click();
+
+        IWebElement hoursDropdownButton = driver.FindElement(By.XPath("//select[@name='availabiltyHour']"));
+        hoursDropdownButton.Click();
+
+        IWebElement hoursTypeOption = driver.FindElement(By.XPath("//option[contains(text(), 'Less than 30hours a week') and @value='0']"));
+        hoursTypeOption.Click();
+
+        //IWebElement hoursTypeOption = driver.FindElement(By.XPath("//option[contains(text(), 'More than 30hours a week') and @value='1']"));
+        //hoursTypeOption.Click();
+
+        //IWebElement hoursTypeOption = driver.FindElement(By.XPath("//option[contains(text(), 'As needed') and @value='2']"));
+        //hoursTypeOption.Click();
+
+    }
+
+}
