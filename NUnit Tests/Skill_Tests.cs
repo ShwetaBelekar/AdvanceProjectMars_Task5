975e2cf68bc88117d15e7435bcc27c470afa36f5
Shweta Belekar <belekarshwetagmail.com>
2025-09-25 12:36:30 +1200

    Created json file for skill feature

--- NUnit Tests/Skill_Tests.cs
+++ NUnit Tests/Skill_Tests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AdvanceProjectMars_Task5.Pages;
+using AdvanceProjectMars_Task5.Utilities;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,20 @@ using System.Threading.Tasks;
 
 namespace AdvanceProjectMars_Task5.NUnit_Tests
 {
-    internal class Skill_Tests
+    [TestFixture]
+    public class Skill_Tests :CommonDriver
     {
+        [SetUp]
+        public void SetUpSteps()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:5003/Home");
+            driver.Manage().Window.Maximize();
+            LoginPage loginPageObj = new LoginPage();
+            loginPageObj.LoginActions();
+            loginPageObj.VerifyUserInHomePage();
+            HomeToSkillPage homeToSkillPageObj = new HomeToSkillPage();
+            homeToSkillPageObj.NavigateToSkill();
+        }
     }
 }
---
+++ TestData/Skill/SkillTestData.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceProjectMars_Task5.TestData.Skill
+{
+    public class SkillTestData
+    {
+        public class SkillData
+        {
+            
+        }
+
+        public static class TestDataReader
+        {
+            public static Dictionary<string, List<SkillData>> ReadTestData(string fileName)
+            {
+                string projectRoot = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", ".."));
+                string testDataPath = Path.Combine(projectRoot, "Configuration", "Skill_TestCases", fileName);
+                string jsonData = File.ReadAllText(testDataPath);
+                var testData = JsonConvert.DeserializeObject<Dictionary<string, List<SkillData>>>(jsonData);
+                return testData;
+            }
+        }
+    }
+}
