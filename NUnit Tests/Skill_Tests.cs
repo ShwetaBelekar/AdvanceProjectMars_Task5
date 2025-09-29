9ac33534d3718a8933261fdb34026cac2b1c8573
Shweta Belekar <belekarshwetagmail.com>
2025-09-29 13:18:45 +1300

    Automated test cases for skill feature

--- NUnit Tests/Skill_Tests.cs
+++ NUnit Tests/Skill_Tests.cs
@@ -1,12 +1,15 @@
 ﻿using AdvanceProjectMars_Task5.Pages;
 using AdvanceProjectMars_Task5.Utilities;
 using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using RazorEngine;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static AdvanceProjectMars_Task5.TestData.Skill.SkillTestData;
 
 namespace AdvanceProjectMars_Task5.NUnit_Tests
 {
@@ -25,5 +28,152 @@ namespace AdvanceProjectMars_Task5.NUnit_Tests
             HomeToSkillPage homeToSkillPageObj = new HomeToSkillPage();
             homeToSkillPageObj.NavigateToSkill();
         }
+        public static IEnumerable<TestCaseData> GetTestData(string fileName, string testName)
+        {
+            var testData = TestDataReader.ReadTestData(fileName);
+            foreach (var data in testData[testName])
+            {
+                if (data.Skill != null && data.Level != null && data.NewSkill != null && data.NewLevel != null)
+                {
+                    yield return new TestCaseData(data.Skill, data.Level, data.NewSkill, data.NewLevel);
+                }
+                else if (data.Skill != null && data.Level != null && data.EditSkill != null)
+                {
+                    yield return new TestCaseData(data.Skill, data.Level, data.EditSkill);
+                }
+                else if (data.Skill != null && data.Level != null)
+                {
+                    yield return new TestCaseData(data.Skill, data.Level);
+                }
+
+            }
+
+        }
+        [Test, TestCaseSource(nameof(GetTestData), new object[] { "skill_validskillandlevel.json", "Createvalidskillandlevelrecord" })]
+        public void Createvalidskillandlevelrecord(string Skill, string Level)
+        {
+            SkillPage skillPageObj = new SkillPage();
+            skillPageObj.CreateSkillRecord(Skill, Level);
+            Console.WriteLine($"Selected {Skill} {Level}");
+            Wait.WaitToBeClickable(driver, "XPath", "//div[@class='ns-box ns-growl ns-effect-jelly ns-type-success ns-show']", 2);
+            IWebElement popupAlert = driver.FindElement(By.XPath("//div[@class='ns-box ns-growl ns-effect-jelly ns-type-success ns-show']"));
+            string promptText = popupAlert.Text;
+            Console.WriteLine("Alert text: " + promptText);
+            IWebElement newSkill = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[1]"));
+            IWebElement newLevel = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[2]"));
+
+            if (newSkill.Text == Skill && newLevel.Text == Level)
+            {
+                Assert.Pass("record created successfully");
+            }
+            else
+            {
+                Assert.Fail("record creation unsuccessful");
+            }
+
+        }
+
+        [Test, TestCaseSource(nameof(GetTestData), new object[] { "skill_invalidskillandlevel.json", "Createinvalidskillandlevelrecord" })]
+        public void Createinvalidskillandlevelrecord(string Skill, string Level)
+        {
+            SkillPage skillPageObj = new SkillPage();
+            skillPageObj.CreateSkillRecord(Skill, Level);
+            Console.WriteLine($"Selected {Skill} {Level}");
+            Wait.WaitToBeClickable(driver, "XPath", "//div[@class='ns-box ns-growl ns-effect-jelly ns-type-success ns-show']", 2);
+            IWebElement popupAlert = driver.FindElement(By.XPath("//div[@class='ns-box ns-growl ns-effect-jelly ns-type-success ns-show']"));
+            string promptText = popupAlert.Text;
+            Console.WriteLine("Alert text: " + promptText);
+            IWebElement newSkill = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[1]"));
+            IWebElement newLevel = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[2]"));
+
+            if (newSkill.Text == Skill && newLevel.Text == Level)
+            {
+                Assert.Pass("System is accepting invalid data, which is incorrect.");
+            }
+            else
+            {
+                Assert.Fail("System is not accepting invalid data, which is correct.");
+            }
+
+        }
+        [Test, TestCaseSource(nameof(GetTestData), new object[] { "skill_blankskill.json", "Createblankskillrecord" })]
+        public void Createblankskillrecord(string Skill, string Level)
+        {
+            SkillPage skillPageObj = new SkillPage();
+            skillPageObj.CreateSkillRecord(Skill, Level);
+            Console.WriteLine($"Selected {Skill} {Level}");
+            Wait.WaitToBeClickable(driver, "XPath", "//div[@class='ns-box ns-growl ns-effect-jelly ns-type-error ns-show']", 2);
+            IWebElement popupAlert = driver.FindElement(By.XPath("//div[@class='ns-box ns-growl ns-effect-jelly ns-type-error ns-show']"));
+            string promptText = popupAlert.Text;
+            Console.WriteLine("Alert text: " + promptText);
+            if (popupAlert.Text == "Please enter skill and experience level")
+            {
+                Assert.Pass("Blank skill record not accepted");
+            }
+            else
+            {
+                Assert.Fail("Blank skill record accepted");
+            }
+
+        }
+        [Test, TestCaseSource(nameof(GetTestData), new object[] { "skill_blanklevel.json", "Createblanklevelrecord" })]
+        public void Createblanklevelrecord(string Skill, string Level)
+        {
+            SkillPage skillPageObj = new SkillPage();
+            skillPageObj.CreateSkillRecord(Skill, Level);
+            Console.WriteLine($"Selected {Skill} {Level}");
+            Wait.WaitToBeClickable(driver, "XPath", "//div[@class='ns-box ns-growl ns-effect-jelly ns-type-error ns-show']", 2);
+            IWebElement popupAlert = driver.FindElement(By.XPath("//div[@class='ns-box ns-growl ns-effect-jelly ns-type-error ns-show']"));
+            string promptText = popupAlert.Text;
+            Console.WriteLine("Alert text: " + promptText);
+            if (popupAlert.Text == "Please enter skill and experience level")
+            {
+                Assert.Pass("Blank level record not accepted");
+            }
+            else
+            {
+                Assert.Fail("Blank level record accepted");
+            }
+
+        }
+        [Test, TestCaseSource(nameof(GetTestData), new object[] { "skill_duplicateskillandlevel.json", "Createduplicateskillrecord" })]
+        public void Createduplicateskilllrecord(string Skill, string Level)
+        {
+            SkillPage skillPageObj = new SkillPage();
+            skillPageObj.CreateSkillRecord(Skill, Level);
+            Console.WriteLine($"Selected {Skill} {Level}");
+            Wait.WaitToBeClickable(driver, "XPath", "//div[@class='ns-box ns-growl ns-effect-jelly ns-type-success ns-show']", 2);
+            IWebElement popupAlert = driver.FindElement(By.XPath("//div[@class='ns-box ns-growl ns-effect-jelly ns-type-success ns-show']"));
+            string promptText = popupAlert.Text;
+            Console.WriteLine("Alert text: " + promptText);
+            Thread.Sleep(3000);
+            IWebElement newSkill = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[1]"));
+            IWebElement newLevel = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[2]"));
+
+            if (newSkill.Text == Skill && newLevel.Text == Level)
+            {
+                Console.WriteLine("record created successfully");
+            }
+            else
+            {
+                Console.WriteLine("record creation unsuccessful");
+            }
+            skillPageObj.CreateSkillRecord(Skill, Level);
+            Console.WriteLine($"Selected {Skill} {Level}");
+            Wait.WaitToBeClickable(driver, "XPath", "//div[@class='ns-box ns-growl ns-effect-jelly ns-type-error ns-show']", 2);
+            IWebElement poopupAlert = driver.FindElement(By.XPath("//div[@class='ns-box ns-growl ns-effect-jelly ns-type-error ns-show']"));
+            string prromptText = poopupAlert.Text;
+            Console.WriteLine("Alert text: " + prromptText);
+            if (poopupAlert.Text == "This skill is already exist in your skill list.")
+            {
+                Assert.Pass("Duplicate record not accepted");
+            }
+            else
+            {
+                Assert.Fail("Duplicate record accepted");
+            }
+        }
+
+
     }
 }
--- Pages/SkillPage.cs
+++ Pages/SkillPage.cs
@@ -15,18 +15,20 @@ namespace AdvanceProjectMars_Task5.Pages
         private IWebElement chooseSkillLevelDropdownButton => driver.FindElement(By.XPath("//select[@name='level']"));
         private IWebElement addButton => driver.FindElement(By.XPath("//input[@value='Add']"));
         private IWebElement cancelButton => driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/div/span/input[2]"));
-        private IWebElement editButton => driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody/tr/td[3]/span[1]/i"));
+        private IWebElement editButton => driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[3]/span[1]/i"));
         private IWebElement updateButton => driver.FindElement(By.XPath("//input[@value='Update']"));
         private IWebElement editCancelButton => driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody/tr/td/div/span/input[2]"));
-        private IWebElement deleteButton => driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody/tr/td[3]/span[2]/i"));
-        public void CreateSkillRecord()
+        private IWebElement deleteButton => driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[3]/span[2]/i"));
+        public void CreateSkillRecord(string Skill, string Level)
         {
             
             addNewButton.Click();
 
             addSkillTextbox.Click();
+            addSkillTextbox.SendKeys(Skill);
 
             chooseSkillLevelDropdownButton.Click();
+            chooseSkillLevelDropdownButton.SendKeys(Level);
 
             //IWebElement skillLevelOption = driver.FindElement(By.XPath("//option[@value='Beginner']"));
             //skillLevelOption.Click();
@@ -35,18 +37,69 @@ namespace AdvanceProjectMars_Task5.Pages
             //IWebElement skillLevelOption = driver.FindElement(By.XPath("//option[@value='Expert']"));
             //skillLevelOption.Click();
             addButton.Click();
-            cancelButton.Click();  
+            //cancelButton.Click();  
 
         }
-
-        public void EditSkillRecord()
+        public void CreateDuplicateRecord(string Skill,string Level)
         {
-            editButton.Click();
+            Thread.Sleep(5000);
+            addNewButton.Click();
 
+            addSkillTextbox.Click();
+            addSkillTextbox.SendKeys(Skill);
+            Thread.Sleep(3000);
+            chooseSkillLevelDropdownButton.Click();
+            chooseSkillLevelDropdownButton.SendKeys(Level);
+            addButton.Click();
+            Thread.Sleep(2000);
+        }
+
+        public void EditSkillRecord(string NewSkill, string NewLevel)
+        {
+            Thread.Sleep(5000);
+            editButton.Click();
+            addSkillTextbox.Click();
+            addSkillTextbox.SendKeys(NewSkill);
+            Thread.Sleep(2000);
+            chooseSkillLevelDropdownButton.Click();
+            chooseSkillLevelDropdownButton.SendKeys(NewLevel);
+            Thread.Sleep(2000);
             updateButton.Click();
 
-            editCancelButton.Click();  
+            //editCancelButton.Click();  
 
+        }
+        public void CancellingAnEditOperation(string EditSkill)
+        {
+            Thread.Sleep(5000);
+            editButton.Click();
+            addSkillTextbox.Click();
+            addSkillTextbox.SendKeys(EditSkill);
+            Thread.Sleep(3000);
+            editCancelButton.Click();
+            Thread.Sleep(2000);
+            IWebElement Skill = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[1]"));
+            if (Skill.Text == "Yoga")   
+            {
+                Console.WriteLine("Edit operation is cancelled and original Skill Yoga is visible");
+            }
+            else
+            {
+                Console.WriteLine("Edit operation is cancelled but original Skill Yoga is not visible");
+            }
+            editButton.Click();
+            Thread.Sleep(2000);
+            updateButton.Click();
+            Thread.Sleep(2000);
+            IWebElement newSkill = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[1]"));
+            if (newSkill.Text == "Calligraphy")
+            {
+                Console.WriteLine("Edit operation was cancelled so the data should be discarded and original Skill Yoga should be visible but no the system is saving unnecessary data at the back");
+            }
+            else
+            {
+                Console.WriteLine("Calligraphy is correct system is not saving any unnecessary data");
+            }
         }
         public void DeleteSkillRecord()
         {
